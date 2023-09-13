e2e11aede8b12e0d24da07ea364e370773990716
Kelisei <kelisei@Debian.kelisei.com>
2023-09-13 16:53:20 -0300

    Zapallo

--- Ej14.sh
+++ Ej14.sh
@@ -1,23 +1,22 @@
-mv "$file" "$1/$3$(basename "$file")"#!/bin/bash
-
 if [ $# -ne 3 ]
 then
     echo "Faltaron argumentos pa"
     exit 1
 fi
 
-if [ -e "$1" ]
+if [ -e $1 ]
 then
-    if [ -d "$1" ]
+    if [ -d $1 ]
     then
-        for file in "$1"/*
+        for file in $1/*
         do
-            if [ "$2" = "-a" ]
+	    echo $file
+            if [ $2 = "-a" ]
             then
-                mv "$file" "$3 $(basename "$file")"
-            elif [ "$2" = "-b" ]
+                mv $file  $file$3
+            elif [ $2 = "-b" ]
             then
-                mv "$file" "$(basename "$file") $3"
+                mv $file $3$file
             else
                 echo "No es una opcion"
             fi
@@ -27,5 +26,5 @@ then
     fi
 else
     echo "No existe el directorio"
-    echo "El argumento 1 deberia ser el directorio, el 2 after -a  before -b, y el 3 el agregado"
+    echo "El argumento 1 deberia ser el directorio, el 2 after -a before -b, y el 3 el agregado"
 fi
