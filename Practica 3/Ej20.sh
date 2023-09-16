7257cc8ad26da4147f85a3d07f3609e63bf096cb
Kelisei <kelisei@Debian.kelisei.com>
2023-09-16 16:43:56 -0300

    20

---
+++ Practica 3/Ej20.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+<<Ej20
+20. Realice un script que simule el comportamiento de una estructura de PILA e implemente
+las siguientes funciones aplicables sobre una estructura global definida en el script:
+push: Recibe un parámetro y lo agrega en la pila 
+pop: Saca un elemento de la pila
+length: Devuelve la longitud de la pila 
+print: Imprime todos elementos de la pila
+Ej20
+
+length=0
+queue=()
+pop() {
+    if [ $length -gt 0 ]; then
+        length=$((length - 1))
+        #echo "Nuevo tamaño $length"
+        local ret_value=${queue[length]}
+        unset 'queue[length]'
+        queue=("${queue[@]}")
+        echo $ret_value
+        return 0
+    else
+        echo "La cola está vacía"
+        return 1
+    fi
+}
+push(){
+    new_element=$1
+    queue[length]=$new_element
+    length=$((length + 1))
+    return 0
+}
+print(){
+    for elem in ${queue[@]}; do
+        echo $elem
+    done
+    return 0
+}
+length(){
+    echo $length
+}
+
+
+push 32
+push 23
+push goku
+pop 
+pop
+pop
+pop
+print
+push 42
+push zapallo
+print
+length
