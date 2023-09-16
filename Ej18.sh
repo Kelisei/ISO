f37f680ed47f79f1009f706f6603491e5b353bdf
Kelisei <kelisei@Debian.kelisei.com>
2023-09-16 14:58:24 -0300

    Dieciocho

---
+++ Ej18.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+<<Ejercicio_18
+18. Crear un script que verifique cada 10 segundos si un usuario se ha loqueado en el sistema
+(el nombre del usuario será pasado por parámetro). Cuando el usuario finalmente se loguee,
+el programa deberá mostrar el mensaje ”Usuario XXX logueado en el sistema” y salir.
+Ejercicio_18
+if ! [ $# -eq 1 ]; then
+    echo "Se necesita almenos 1 parametro (nombre de usuario)" >&2
+    exit 1
+fi
+while true; do 
+    expr $(who | grep -w $1 | wc -l) > 0
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    echo "No encontrado, esperando 10 segundos"
+    sleep 10
+done
+echo "Usuario $1 logueado en el sistema"
